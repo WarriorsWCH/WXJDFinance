c6911446f13033de86a18d12dbc7f3b2b69c4c79
groot <328592171@qq.com>
2018-03-14 18:11:31 +0800

    feat(common): slider

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,16 @@
-Page({})
+Page({
+  data: {
+    swipers: {
+      imgUrls: [
+        'https://img12.360buyimg.com/jrpmobile/jfs/t14644/273/2524609793/42501/a6e9640e/5aa7b920Nbb6aced0.jpg?width=750&height=320',
+        'https://img12.360buyimg.com/jrpmobile/jfs/t18175/250/764323238/50430/e40edddb/5aa7b944N16aea43d.jpg?width=750&height=320',
+        'https://img12.360buyimg.com/jrpmobile/jfs/t18619/231/784925086/42554/22281623/5aa7b8f7N8fd57e2c.jpg?width=750&height=320'
+      ],
+      indicatorDots: true,
+      autoplay: true,
+      circular: true,
+      interval: 5000,
+      duration: 1000
+    }
+  }
+})
