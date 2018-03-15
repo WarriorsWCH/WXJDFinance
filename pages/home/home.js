ae962688bbe2cbbff8b7173b8b203087d541211a
groot <328592171@qq.com>
2018-03-15 16:36:05 +0800

    feat(common): classify-item

--- pages/home/home.js
+++ pages/home/home.js
@@ -11,6 +11,53 @@ Page({
       circular: true,
       interval: 5000,
       duration: 1000
+    },
+    topItems: {
+      items: [
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t3991/64/2521945388/12110/93c0139/58d1e462Ncf294123.png?width=132&height=132",
+          title: "优惠券"
+        },
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t5590/252/875247023/17343/946aa72c/59224650N0f7ffc92.png?width=132&height=132",
+          title: "领红包"
+        },
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t4393/329/2180608902/13217/c88c0cec/58ec9dcdN1534e2d7.png?width=132&height=132",
+          title: "抢钢镚",
+          color: 'red'
+        },
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t5488/298/1036263348/12073/b4f4de97/590ac8e8Ne9def22e.png?width=135&height=135",
+          title: "白条提额"
+        }
+      ],
+      num: '4'
+    },
+    bottomItems: {
+      items: [
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t12799/50/704102454/5247/19eac80a/5a4f2622Nc4aca701.png?width=90&height=90",
+          title: "旅游"
+        },
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t16033/196/1306232362/4705/4c678227/5a4f265bN66ef5ae8.png?width=90&height=90",
+          title: "医美"
+        },
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t13051/63/2559290659/4702/e4068917/5a4f2693N937f1905.png?width=88&height=88",
+          title: "房抵贷"
+        },
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t15133/16/1440686097/5786/1b038e03/5a4f26bfN03ed1f66.png?width=90&height=90",
+          title: "驾校"
+        },
+        {
+          imgUrl: "https://img12.360buyimg.com/jrpmobile/jfs/t14896/125/1435676873/3668/a8b392f7/5a4f26edN6cdfda66.png?width=88&height=88",
+          title: "分期用"
+        },
+      ],
+      num: '5'
     }
   }
 })
